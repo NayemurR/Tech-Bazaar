5087d5f18d9be0a69f9ba9894d36f51bf6f35c00
NayemurR <76692438+NayemurR@users.noreply.github.com>
2020-12-28 20:22:29 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Tech Bazaar
+This is a website for a mobile phone shop. The project is developed using HTML, CSS, Javascript, Jquery, PHP, MySQL. Also I have used owl carsoul at different parts of the project.  
---
+++ Template/_blogs.php
@@ -0,0 +1,38 @@
+
+<section id="blogs">
+    <div class="container py-4">
+        <h4 class="font-rubik font-size-20">Latest Blogs</h4>
+        <hr>
+
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
+                    <img src="./assets/blog/blog1.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">At the Mi 11 series launch event Xiaomi also introduced the MIUI 12.5, the next update after the MIUI 12 was introduced back in April this year.  MIUI 12.5 is lighter, faster and more economical, says Xiaomi. System UI is rewritten, so the memory usage is reduced by 20% and the background memory usage is less by an average of 35%, and the system application power consumption decreased by an average of 25%, adds Xiaomi.</p>
+                    <a href="#" class="color-second text-left">Read more</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
+                    <img src="./assets/blog/blog2.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">The next iPhone XII, we’ll see an OLED screen and 5G as standard.
+
+In the next line of apple 2020 smartphones, As Artificial intelligence grows, Apple is working on a new series of chips called the “bionic chips”. These bionic chips were meant for aiding boost the AI-powered processes. Maybe we could see more functionality from the iphone XII as the technological world around smartphones also continues to grow.
+
+We may also see different camera modes and even better AI-powered camera features.</p>
+                    <a href="#" class="color-second text-left">Read more</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
+                    <img src="./assets/blog/blog3.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">One of the best upcoming smartphone in 2020 from Samsung Galaxy-S series is expected to be the new Samsung Galaxy S11 with some features of Samsung Galaxy Note 10 like the 45W fast charging and an even better camera than the Samsung galaxy s10 series. According to reports, the Samsung galaxy s11 will take the route of apple and decide to remove the headphone jack. The galaxy s10 front facing camera will be under the display similar to the onePlus 7 and onePlus 7T front facing cameras.</p>
+                    <a href="#" class="color-second text-left">Read more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
